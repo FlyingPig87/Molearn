a852dd914f5682e82782ec3e64c02f288cf47772
FlyingPig87 <geniuscj1987@yahoo.com.cn>
2013-03-19 15:04:31 +0800

    Initial Commit

---
+++ Molearn/MLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MLAppDelegate.h
+//  Molearn
+//
+//  Created by chen jian on 3/19/13.
+//  Copyright (c) 2013 cj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Molearn/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Molearn
+//
+//  Created by chen jian on 3/19/13.
+//  Copyright (c) 2013 cj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MLAppDelegate class]));
+    }
+}
